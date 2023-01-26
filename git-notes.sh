ae32fd7948a598436a360d90c5805fd0bf6226ee
JenieX <sozorablack@gmail.com>
2023-01-26 04:57:06 +0300

    br2 commit

---
+++ git-notes.sh
@@ -0,0 +1,24 @@
+exit
+
+# Create branch
+git branch <branchName>
+
+# Swich to branch
+git checkout <branchName>
+
+# Create and switch to branch
+git chechout -b <branchName>
+
+# List branches
+git branch -a
+git branch --all
+
+# Delete branch
+git branch -d <branchName>
+git branch --delete <branchName>
+
+# Delete remote branch
+git push -d <remoteName> <branchName>
+git push --delete <remoteName> <branchName>
+
+# On br2 !
